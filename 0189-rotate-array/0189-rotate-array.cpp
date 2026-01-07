95a8579b90e0e7f04dc2c9154e9b0ee678da2e4b
Viraj Jamdhade <virajjamdhade6@gmail.com>
2026-01-07 15:47:09 +0530

    Time: 0 ms (100%), Space: 29.5 MB (64.28%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -8,7 +8,7 @@ public:
         //Handling K
         // Total rotations without optimization = length of linked list + k
          // Here we can see that we can get our expected output just doing k rotations, no need of length of linked list while for rotations
-         k = k % length     // returns optimized count or index
+         k = k % length;     // returns optimized count or index
 
          // Step 1 - reverse entire array
          reverse(nums.begin(), nums.end());
