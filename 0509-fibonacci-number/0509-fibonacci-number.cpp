02d82458f438389f6ba9db2a9b591b047acab678
Viraj Jamdhade <virajjamdhade6@gmail.com>
2026-02-14 10:04:13 -0800

    Time: 13 ms (31.9%), Space: 7.8 MB (36.2%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -2,11 +2,13 @@ class Solution {
 public:
     int fib(int n) 
     {
+        //if n is 0 or 1 return n itself
         if(n <= 1)
         {
             return n;
         }    
         
+        // recursive call : addition of previous two numbers
         return fib(n-1) + fib(n - 2);
     }
 };
