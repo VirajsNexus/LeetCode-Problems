59c8b650574cb62b00306d0d8c4b3681336d6dd6
Viraj Jamdhade <virajjamdhade6@gmail.com>
2026-03-04 16:36:19 +0000

    Time: 0 ms (100%), Space: 8.6 MB (44.32%) - LeetHub

---
+++ 0258-add-digits/0258-add-digits.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int addDigits(int num) 
+    {
+        while(num >= 10)    // repeat until single digit
+        {   
+            int sum = 0;
+            
+            while(num > 0) {
+                sum += num % 10;  // get last digit
+                num /= 10;        // remove last digit
+            }
+            
+            num = sum;
+        }
+        
+        return num;
+    }
+};
