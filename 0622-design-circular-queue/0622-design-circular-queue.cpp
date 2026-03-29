13c8758fc2a86a8694bcab0f3d8006fe3f3b5e02
Viraj Jamdhade <virajjamdhade6@gmail.com>
2026-03-29 23:32:03 +0530

    Time: 7 ms (19.29%), Space: 23.6 MB (9.17%) - LeetHub

---
+++ 0622-design-circular-queue/0622-design-circular-queue.cpp
@@ -0,0 +1,52 @@
+class MyCircularQueue {
+private:
+    int *arr;
+    int front;
+    int rear;
+    int size;
+    int capacity;
+
+public:
+    MyCircularQueue(int k) {
+        capacity = k;
+        arr = new int[k];
+        front = 0;
+        rear = -1;
+        size = 0;
+    }
+    
+    bool enQueue(int value) {
+        if (isFull()) return false;
+        
+        rear = (rear + 1) % capacity;
+        arr[rear] = value;
+        size++;
+        return true;
+    }
+    
+    bool deQueue() {
+        if (isEmpty()) return false;
+        
+        front = (front + 1) % capacity;
+        size--;
+        return true;
+    }
+    
+    int Front() {
+        if (isEmpty()) return -1;
+        return arr[front];
+    }
+    
+    int Rear() {
+        if (isEmpty()) return -1;
+        return arr[rear];
+    }
+    
+    bool isEmpty() {
+        return size == 0;
+    }
+    
+    bool isFull() {
+        return size == capacity;
+    }
+};
