82930966bfc868f8a22a1ed9e3083917decae432
Viraj Jamdhade <virajjamdhade6@gmail.com>
2026-04-09 21:13:19 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1025-divisor-game/1025-divisor-game.cpp
+++ 1025-divisor-game/1025-divisor-game.cpp
@@ -2,6 +2,8 @@ class Solution {
 public:
     bool divisorGame(int n) 
     {
+        // If number is even → Alice wins
+        // If number is odd  → Alice loses
         return n % 2 == 0;
     }
 };
