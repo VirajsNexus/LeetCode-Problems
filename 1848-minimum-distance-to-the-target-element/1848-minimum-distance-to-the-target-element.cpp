cf2879b66d62a750e1fb55e92858b40fa68eca2f
Viraj Jamdhade <virajjamdhade6@gmail.com>
2026-04-13 22:00:31 +0530

    Time: 0 ms (100%), Space: 13.2 MB (51.84%) - LeetHub

--- 1848-minimum-distance-to-the-target-element/1848-minimum-distance-to-the-target-element.cpp
+++ 1848-minimum-distance-to-the-target-element/1848-minimum-distance-to-the-target-element.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int getMinDistance(vector<int>& nums, int target, int start) 
     {
-        int ans = INT_MAX;
+        int ans = INT_MAX;  //largest possible integer value(used for finding minimum and as worst case,beacause I havent found any answer yet thats why not 0)
 
         for(int i = 0; i < nums.size(); i++)
         {
