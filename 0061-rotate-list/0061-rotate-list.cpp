e1522620813f7480044b5bfac452c500c8ec65a2
Viraj Jamdhade <virajjamdhade6@gmail.com>
2026-05-05 23:10:41 +0530

    Time: 0 ms (100%), Space: 16.5 MB (31.2%) - LeetHub

--- 0061-rotate-list/0061-rotate-list.cpp
+++ 0061-rotate-list/0061-rotate-list.cpp
@@ -12,67 +12,41 @@ class Solution {
 public:
     ListNode* rotateRight(ListNode* head, int k) 
     {
-        //Two pointers Technique
-
-        // Case 1 - Empty List or Single node linked list
         if(NULL == head || NULL == head -> next)
-        {
             return head;
-        }
 
-        // Now We know that linked list is present and having 2 or more nodes
-        //calculate length of Linked List for travesing purpose
-        ListNode* Current = head;       //Current ptr initialized and placed at head
-        int length = 0;                 // initially length  = 0
-        while(Current)
+        //find length of list
+        int n = 0;
+        ListNode* temp = head;
+        while(temp != NULL)
         {
-            ++length;
-            Current = Current ->next;
-        }
-         //Handling 'k'
-         // Total rotations without optimization = length of linked list + k
-         // Here we can see that we can get our expected output just doing k rotations, no need of length of linked list while for rotations
+            n++;
+            temp = temp -> next;
+        }    
 
-         // optimized K
-         k %= length;       // returns optimized count of linked list that we need
-
-        if(k == 0)          // no need of rotation 
-        {
+        //minimize repetitive rotations
+        k = k % n;
+        if(k == 0)
             return head;
-        }
-
-        // finding rotation point using two pointers first and last respectively
-        //Initialize First and Last Pointers
-        ListNode* First = head;
-        ListNode* Last = head;
-
-
-        // move First pointer k steps ahead to create gap
+        
+        //final k times rotation
         while(k--)
         {
-            First = First -> next;
+            ListNode* previous = NULL;
+            ListNode* current = head;
+
+            //traverse to last node
+            while(current -> next != NULL)
+            {
+                previous = current;
+                current = current -> next;
+            }
+
+            //Last becomes first
+            previous -> next = NULL;
+            current -> next = head;
+            head = current;
         }
-
-        // move both pointers together untill first reaches to the last node
-        while(First -> next)
-        {
-            First = First -> next;
-            Last = Last -> next;
-        }
-
-        // Now, First is at last node and Last is at node before new head 
-
-        // Actual Rotation
-        //Place new head after Last Pointer
-        ListNode* newHead= Last -> next;
-
-        // remove link at the point of rotation means remove contact between Last and newHead
-        // Hence, This will become last node of rotated list
-        Last -> next = nullptr;
-
-        //connect initial last node to first node to complete the linked list
-        First -> next = head;
-
-         return newHead;        // rotated linked list
+        return head;
     }
 };
