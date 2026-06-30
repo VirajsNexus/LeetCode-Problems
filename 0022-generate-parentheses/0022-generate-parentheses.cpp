5bc6731f299578b47c78a4e063d8be4165157489
Viraj Jamdhade <virajjamdhade6@gmail.com>
2026-06-30 14:57:35 +0530

    Time: 0 ms (100%), Space: 13.6 MB (72.5%) - LeetHub

---
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    vector<string> result;
+
+    void backTrack(string &current, int open, int close, int size)
+    {
+        if(current.size() == 2 * size)
+        {
+            result.push_back(current);
+            return;
+        }
+
+        //choose '('
+        if(open < size)
+        {
+            current.push_back('(');
+            backTrack(current, open + 1, close, size);
+            current.pop_back();
+        }
+
+        //Choose ')'
+        if(close < open)
+        {
+            current.push_back(')');
+            backTrack(current, open, close + 1, size);
+            current.pop_back();
+        }
+
+    }
+    vector<string> generateParenthesis(int n) 
+    {
+        string current = "";
+        backTrack(current, 0, 0, n);
+        return result;
+    }
+};
