0734b928ed7106eb4fb6d30b646d5d2d0ae37fb3
Viraj Jamdhade <virajjamdhade6@gmail.com>
2026-07-14 20:45:42 +0530

    Time: 8 ms (83.03%), Space: 11.9 MB (23.47%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.cpp
+++ 0141-linked-list-cycle/0141-linked-list-cycle.cpp
@@ -9,35 +9,25 @@
 class Solution {
 public:
     bool hasCycle(ListNode *head) 
-    {   
-        //Case 1: Empty linked list
-            if(NULL == head)
+    {
+        if(head == NULL || head -> next == NULL)
+        {
+            return false;
+        }    
+
+        ListNode *fast = head;
+        ListNode *slow = head;
+
+        while(fast != NULL && fast -> next != NULL)
+        {
+            slow = slow -> next;
+            fast = fast -> next -> next;
+
+            if(slow == fast)
             {
-                return false;
+                return true;
             }
-        // case 2 : linked list present
-            
-            //Initialize two pointers for traversing linked list
-            ListNode* First = head;
-            ListNode* Last = head;
-
-            // To traverse linked list Both pointers should have some gap between them 
-            // so that it will check for cycle is present or not for all node conditions formed 
-
-            // If if last nodes next returns nullptr it means Cycle is not present
-            while(First != nullptr && First -> next != nullptr)
-            {
-                Last = Last -> next;            // traversing by single node only
-                First = First ->next ->next;  // traversing by two nodes
-
-            // IMP Logic : If cycle is present Fisrt and Last will met eventually
-                if(First == Last)       // Checking for cycle
-                {
-                    // Cycle Found
-                    return true;
-                }
-            }
-
-            return false;       //No cycle Fouund
+        }
+        return false;
     }
 };
