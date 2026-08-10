d6f26bc1016ce1374335177d55b5a30f0d0e5ab9
Viraj Jamdhade <virajjamdhade6@gmail.com>
2026-08-10 21:15:11 +0530

    Time: 34 ms (73.12%), Space: 9 MB (82.55%) - LeetHub

---
+++ 1510-stone-game-iv/1510-stone-game-iv.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool winnerSquareGame(int n) 
+    {
+        vector<bool> dp(n+1, false);
+
+        //dp[0] = false
+        //no stones -> current player cant move -> loses
+        for(int i = 1; i <= n; i++)
+        {
+            for(int j = 1; j * j <= i; j++)
+            {
+                //remove j * j stone
+                //If opponent is in losing state,
+                // current player wins
+                if(dp[i - j * j] == false)
+                {
+                    dp[i] =  true;
+                    break;
+                }
+            }
+        }     
+        return dp[n];
+    }
+};
